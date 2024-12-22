d4b422d0b3d2792b7a099fc5271abcfa11c6e510
ahnet-69 <syedmusicrealise@gmail.com>
2024-12-22 11:11:27 +0000

    scx30g-common: Add goyave support

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter core33g,$(TARGET_DEVICE)),)
+ifneq ($(filter goyave3g goyavewifi,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
